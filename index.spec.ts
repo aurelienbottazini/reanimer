2c3b603d4415960a65777d3a9c6b6906c28cf71a
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2022-06-19 12:12:11 +0200

    feat: deftemplate

--- index.spec.ts
+++ index.spec.ts
@@ -1,4 +1,4 @@
-import { hello } from "./index.ts";
+import { hello } from "./index";
 
 test("Hello world", () => {
   expect(hello()).toBe("Hello world! ");
--- src/template.spec.ts
+++ src/template.spec.ts
@@ -1,6 +1,28 @@
-import { deftemplate } from "./template.ts";
+import { deftemplate } from "./template";
 
-test("deftemplate", () => {
-  expect(deftemplate).toBeInstanceOf(Function);
-  expect(deftemplate()).toBeInstanceOf(Function);
+describe(deftemplate, () => {
+  test("simple case", () => {
+    expect(deftemplate).toBeInstanceOf(Function);
+
+    const template = deftemplate("<head><title></title></head>", [
+      ["head title", "foobar"],
+    ]);
+    expect(template).toBeInstanceOf(Function);
+    expect(template()).toEqual(expect.any(String));
+    expect(template()).toEqual("<head><title>foobar</title></head>");
+  });
+
+  test("advanced case", () => {
+    const template = deftemplate(
+      "<body><article><h1></h1><ul></ul></article></body>",
+      [
+        ["article h1", "foobar"],
+        ["ul", "<li>hello</li>"],
+      ]
+    );
+
+    expect(template()).toEqual(
+      "<body><article><h1>foobar</h1><ul><li>hello</li></ul></article></body>"
+    );
+  });
 });
