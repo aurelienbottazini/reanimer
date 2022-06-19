339106c7c3cb2454061f8ec1964ae8fce9c70fe6
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2022-06-19 12:12:11 +0200

    feat: deftemplate

--- src/template.ts
+++ src/template.ts
@@ -1,3 +1,16 @@
-export function deftemplate() {
-  return () => {};
+import { parse } from "node-html-parser";
+
+export function deftemplate(htmlData: string, mappings: [string, string][]) {
+  return (_context?: Object) => {
+    const parsedHtml = parse(htmlData);
+
+    mappings.forEach(([selector, replacement]) => {
+      const node = parsedHtml.querySelector(selector);
+      if (node) {
+        node.innerHTML = replacement;
+      }
+    });
+
+    return parsedHtml.toString();
+  };
 }
